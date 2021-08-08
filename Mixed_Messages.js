f58ea6c1eebff1918020cb8e15682cbebaba732e
DeepInMind1 <ncls.hfmn@gmail.com>
2021-08-08 22:59:01 +0200

    Final implementation

--- Mixed_Messages.js
+++ Mixed_Messages.js
@@ -1,5 +1,14 @@
 const mixedMessage = {
-    messagePart1: [],
-    messagePart2: [],
-    messagePart3: []
+    _messagePart1: ['I', 'You', 'She', 'He', 'It'],
+    _messagePart2: ['always', 'sometimes', 'often', 'occasionally', 'infrequent'],
+    _messagePart3: ['eat(s) hamburgers.', 'steal(s) lighters in a supermarket.', 'go(es) for a walk with the dog.', 'freak(s) out completely.', 'wash(es) the dishes willingly'],
+    createMessage: function() {
+        let randomMessagePart1 = this._messagePart1[Math.floor(Math.random()*5)];
+        let randomMessagePart2 = this._messagePart2[Math.floor(Math.random()*5)];
+        let randomMessagePart3 = this._messagePart3[Math.floor(Math.random()*5)];
+
+        return randomMessagePart1 + ' ' + randomMessagePart2 + ' ' + randomMessagePart3;
+    }
 };
+
+console.log(mixedMessage.createMessage());
